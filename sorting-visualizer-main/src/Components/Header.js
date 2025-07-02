eda101243baee529678e7718cd7683c04e62e343
Dibya2904 <dibyasambitpradhan@gmail.com>
2025-07-02 18:27:50 +0530

    Add files via upload

---
+++ sorting-visualizer-main/src/Components/Header.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+            <div className="logo">
+                <a>Sorting Algorithm Visualizer</a>
+            </div>
+            <div className="code">
+                <a href="https://github.com/sagarcode28/sorting-visualizer">Check the Code</a>
+            </div>
+        </header>
+    );
+}
